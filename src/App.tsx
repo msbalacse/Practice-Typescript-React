dab0b85a1be756fa5dd03eb189a2bd28747b3be1
Balamurugan <msbalacse@gmail.com>
2024-01-08 07:50:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h5
+        style={{
+          textAlign: 'center',
+          background: 'white',
+          padding: '0.5rem',
+          color: 'black',
+        }}
+      >
+        Main App
+      </h5>
+    </>
+  );
+}
+
+export default App;
